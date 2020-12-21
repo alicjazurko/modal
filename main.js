bc1c4ef7c22b92a089210c80a165e1244640718d
alicjazurko <58707380+alicjazurko@users.noreply.github.com>
2020-12-22 00:19:38 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,9 @@
 const x = document.querySelector('.modal__close');
-const modal = document.getElementById('modal');
-const body = document.querySelector('html');
-const modalChildren = modal.children;
-console.log(modalChildren);
+const window1 = document.querySelectorAll('.js');
+const modal = document.querySelector('.modal');
 
 
 x.addEventListener('click', function() {
     modal.classList.add('close');
 })
 
-// modal.addEventListener('click', function() {
-//     modal.classList.add('close');
-// })
-
-window.addEventListener('click', function(e) {
-    if( e.target != modal) {
-        modal.classList.add('close');
-    }
-})
